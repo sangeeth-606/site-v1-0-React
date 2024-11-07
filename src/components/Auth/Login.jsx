9c0c10694527c73a66476c5c17dd278cb03ae01f
sangeeth-606 <sangeeth999123@gmail.com>
2024-11-07 16:46:36 +0530

    login page

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,9 +1,15 @@
-// import React from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
   const submitHandler = (e) => {
     e.preventDefault();
     console.log("Form Submitted");
+
+    setEmail('');
+    setPassword('');
   };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-slate-800 to-slate-950 text-white">
@@ -12,12 +18,14 @@ const Login = () => {
           onSubmit={ (e) => submitHandler(e) }
           className="flex flex-col space-y-5 p-8 rounded-lg text-white"
         >
-          <input
+          <input value={email}
+          onChange={(e) => setEmail(e.target.value)}
             type="email"
             placeholder="Enter your email"
             className="w-full px-5 py-3 bg-slate-900 bg-opacity-60 border border-slate-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-4 focus:ring-blue-500 transition duration-200"
           />
-          <input
+          <input value={password}
+          onChange={(e) => setPassword(e.target.value)}
             type="password"
             placeholder="Enter your password"
             className="w-full px-5 py-3 bg-slate-900 bg-opacity-60 border border-slate-700 rounded-lg text-white placeholder-gray-400 focus:outline-none focus:ring-4 focus:ring-blue-500 transition duration-200"
